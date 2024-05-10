beac8d9a568719c256611d8e44efdcb13b86a877
MJTSgamer <matttersteege@gmail.com>
2024-05-10 09:47:09 +0200

    Backup, video is working somewhat things needed to be fixed are inside VideoPlayer.js

--- ChemCourses/Controllers/VideoController.cs
+++ ChemCourses/Controllers/VideoController.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using ChemCourses.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
@@ -7,6 +8,20 @@ using Newtonsoft.Json;
 public class VideoController : Controller
 {
     private const string VideoPath = @"C:\Users\mattt\RiderProjects\ChemCourses\ChemCourses\wwwroot\videos";
+    
+    [HttpGet("{guid}/manifest")]
+    public IActionResult GetManifest(string guid)
+    {
+        #if DEBUG
+        //no caching in debug mode
+        Response.Headers.Add("Cache-Control", "no-cache, no-store, must-revalidate");
+        Response.Headers.Add("Pragma", "no-cache");
+        Response.Headers.Add("Expires", "0");
+        #endif
+        
+        var path = Path.Combine(VideoPath, guid, "manifest.json");
+        return PhysicalFile(path, "application/json");
+    }
     
     [HttpGet("{guid}/video/{segment}")]
     public IActionResult GetVideoSegment(string guid, string segment)
@@ -34,5 +49,21 @@ public class VideoController : Controller
         
         var path = Path.Combine(VideoPath, guid, @"audio\und\mp4a.40.2", segment);
         return PhysicalFile(path, "application/octet-stream");
+    }
+    
+    [HttpGet("{guid}/mpd")]
+    public IActionResult GetMpd(string guid)
+    {
+        #if DEBUG
+        //no caching in debug mode
+        Response.Headers.Add("Cache-Control", "no-cache, no-store, must-revalidate");
+        Response.Headers.Add("Pragma", "no-cache");
+        Response.Headers.Add("Expires", "0");
+        #endif
+        
+        MpdParser parser = new MpdParser();
+        VideoMPD mpd = parser.Parse(Path.Combine(VideoPath, guid, "stream.mpd"));
+        
+        return Content(JsonConvert.SerializeObject(mpd), "application/json");
     }
 }
--- ChemCourses/wwwroot/js/VideoPlayer.js
+++ ChemCourses/wwwroot/js/VideoPlayer.js
@@ -1,80 +1,214 @@
-﻿//need to implement the following functions
-// - First fetch the amount of segments (video and audio) from the server /api/Video/{videoId}
-// - turn into a 'VideoPlayer' class
+﻿//When you minify the JS code use this website: https://www.digitalocean.com/community/tools/minify, with the following settings:
+//  - Compress
+//      - dead_code, drop_console, drop_debugger
+//  - Mangle
+//      - keep_classnames, keep_fnames, toplevel, safari10
 
-var videoId;
+//need to implement the following functions
+// - First fetch the amount of segments (video and audio) from the server /api/Video/{videoId} (done)
+// - When user adjusts the video time and the time is outside the buffer, fetch needed segment (in progress) need the /{guid}/mpd endpoint for segment lengths
 
-var mimeType_video = "video/mp4";
-var codecs_video = "avc1.4D401F";
-var mimeCodec_video = mimeType_video + ';codecs = "' + codecs_video + '"';
 
-var mimeType_audio = "audio/mp4";
-var codecs_audio = "mp4a.40.2";
-var mimeCodec_audio = mimeType_audio + ';codecs = "' + codecs_audio + '"';
+class VideoPlayer {
+    
+    constructor(video_id) {
+        //set by user - shallow
+        this.videoId = video_id;
+        this.minBuffer = 5; //seconds
+        
+        //set by user - deep
+        this.mimeCodec_audio = "audio/mp4; codecs = mp4a.40.2";
+        this.mimeCodec_video = "video/mp4; codecs = avc1.4D401F";
+        
+        //set / changed internally
+        this.mediaSource;
+        this.sourceBuffer_video;
+        this.sourceBuffer_audio;
+        this.maxSegNum_audio = 1;
+        this.segNum_audio = 1;
+        this.maxSegNum_video = 1;
+        this.segNum_video = 1;
 
-var mediaSource = new MediaSource();
-var sourceBuffer_video;
-var sourceBuffe_audio;
-var segNum = 1;
-var maxSegNum = 100;
+        fetch("api/Video/" + this.videoId + "/manifest")
+        .then(response => response.json())
+        .then(data => {
 
-function startup(video_id) {
-    if (MediaSource.isTypeSupported(mimeCodec_video)) {
-        mediaSource.addEventListener("webkitsourceopen", sourceOpen, false);
-        mediaSource.addEventListener('sourceopen', sourceOpen, false);
-        video = document.querySelector('video');
-        video.src = URL.createObjectURL(mediaSource);
-        videoId = video_id;
-    } else {
-        console.error('Unsupported MIME type or codec: ', mimeCodec_video);
+            this.maxSegNum_audio = data.audio.Files;
+            this.maxSegNum_video = data.video.Files;
+
+            VideoLog("Loaded manifest file:", data);
+            this.canPlay = true;
+        });
+        
+        this.canPlay = false;
     }
-}
 
-function sourceOpen(e) {
-    sourceBuffer_video = mediaSource.addSourceBuffer(mimeCodec_video);
-    sourceBuffe_audio = mediaSource.addSourceBuffer(mimeCodec_audio);
+    startup() {
+        
+        VideoLog("VideoPlayer is queued...");
 
-    fetch("api/Video/" + videoId + "/video/init.mp4", function(buf) {
-        sourceBuffer_video.appendBuffer(buf);
-    });
-
-    fetch("api/Video/" + videoId + "/audio/init.mp4", function(buf) {
-        sourceBuffe_audio.appendBuffer(buf);
-    });
-
-    sourceBuffer_video.addEventListener('updateend', loadSegment);
-};
-
-function loadSegment() {
-    if (segNum < maxSegNum) {
-        getAudioSegment(segNum);
-        getVedioSegment(segNum);
-        segNum++;
+        waitForObjectState(() => this.canPlay)
+        .then(() => {
+            VideoLog("VideoPlayer is starting");
+            this.startupInternal();
+        })
+        .catch((error) => {
+            VideoLog("An error occurred:", error);
+        });
     }
-}
 
-function getVedioSegment(segNum) {
-    fetch("api/Video/" + videoId + "/video/seg-" + segNum + ".m4s", function(buf) {
-        console.log("mp4/output/video/avc1/seg-" + segNum + ".m4s");
-        sourceBuffer_video.appendBuffer(buf);
-    });
-}
+    startupInternal() {
+        if (MediaSource.isTypeSupported(this.mimeCodec_video)) {
+            this.mediaSource = new MediaSource();
+            
+            this.mediaSource.addEventListener("webkitsourceopen", (evt) => {
+                VideoLog("MediaSource opened (type of webkitsourceopen)");
+                this.sourceOpen(evt);
+            }, false);
+            
+            this.mediaSource.addEventListener('sourceopen', (evt) => {
+                VideoLog('MediaSource opened (type of sourceopen)');
+                this.sourceOpen(evt);
+            }, false);
+            
+            this.video = document.querySelector('video');
+            this.video.src = URL.createObjectURL(this.mediaSource);
+            this.video.ontimeupdate = () => this.checkBuffer();
+            this.video.oninput = (event) => this.durationChange(event);
+            
+        } else {
+            VideoLog('Unsupported MIME type or codec: ', this.mimeCodec_video);
+        }
+    }
+    
+    sourceOpen(e) {
+        this.sourceBuffer_video = this.mediaSource.addSourceBuffer(this.mimeCodec_video);
+        this.sourceBuffer_video.mode = 'sequence';
+        this.sourceBuffer_audio = this.mediaSource.addSourceBuffer(this.mimeCodec_audio);
+        this.sourceBuffer_audio.mode = 'sequence';
+        var self = this; // Store a reference to 'this' for fetch callback
+        VideoLog('Source buffers created', this.sourceBuffer_video, this.sourceBuffer_audio);
 
-function getAudioSegment(segNum) {
-    fetch("api/Video/" + videoId + "/audio/seg-" + segNum + ".m4s", function(buf) {
-        console.log("mp4/output/audio/und/mp4a.40.2/seg-" + segNum + ".m4s");
-        sourceBuffe_audio.appendBuffer(buf);
-    });
+        var init = 0;
+        
+        this.fetch("api/Video/" + this.videoId + "/video/init.mp4", function (buf) {
+            self.sourceBuffer_video.appendBuffer(buf);
+            init++;
+        });
 
-}
+        this.fetch("api/Video/" + this.videoId + "/audio/init.mp4", function (buf) {
+            self.sourceBuffer_audio.appendBuffer(buf);
+            init++;
+        });
 
-function fetch(url, cb) {
-    console.log(url);
-    var xhr = new XMLHttpRequest;
-    xhr.open('get', url);
-    xhr.responseType = 'arraybuffer';
-    xhr.onload = function() {
-        cb(xhr.response);
+        waitForObjectState(() => init === 2)
+        .then(() => {
+            VideoLog("Init segments loaded");
+            this.loadSegment();
+        });
     };
-    xhr.send();
-};
+    
+    loadSegment() {
+        if (this.segNum_audio <= this.maxSegNum_audio) {
+            this.getAudioSegment(this.segNum_audio);
+            this.segNum_audio++;
+        }
+        
+        if (this.segNum_video <= this.maxSegNum_video) {
+            this.getVideoSegment(this.segNum_video);
+            this.segNum_video++;
+        }
+    }
+    
+    loadSpecificSegment(segNum) {
+        this.getAudioSegment(segNum);
+        this.getVideoSegment(segNum);
+    }
+    
+    getVideoSegment(segNum) {
+        var self = this; // Store a reference to 'this' for fetch callback
+        this.fetch("api/Video/" + this.videoId + "/video/seg-" + segNum + ".m4s", function (buf) {
+            self.sourceBuffer_video.appendBuffer(buf);
+        });
+    }
+    
+    getAudioSegment(segNum) {
+        var self = this; // Store a reference to 'this' for fetch callback
+        this.fetch("api/Video/" + this.videoId + "/audio/seg-" + segNum + ".m4s", function (buf) {
+            self.sourceBuffer_audio.appendBuffer(buf);
+        });
+
+    }
+    
+    fetch(url, cb) {
+        //VideoLog(url);
+        var xhr = new XMLHttpRequest;
+        xhr.open('get', url);
+        xhr.responseType = 'arraybuffer';
+        xhr.onload = function () {
+            cb(xhr.response);
+        };
+        xhr.send();
+    }
+    
+    checkBuffer() {
+        
+        //VideoLog("Checking buffer...");
+        
+        if (this.video.currentTime >= this.video.buffered.end(0) - this.minBuffer) {
+            var logMessage = "Buffer is low, loading ";
+            if (this.segNum_audio <= this.maxSegNum_audio && this.segNum_video <= this.maxSegNum_video) {
+                logMessage += "new audio and video segment";
+            } 
+            else {
+                if (this.segNum_audio <= this.maxSegNum_audio) {
+                    logMessage += "new audio segment";
+                }
+                if (this.segNum_audio > this.maxSegNum_audio && this.segNum_video > this.maxSegNum_video) {
+                    logMessage += "nothing";
+                }
+            }
+            VideoLog(logMessage);
+            this.loadSegment();
+        }
+    }
+    
+    play() {
+        this.video.play();
+    }
+    
+    pause() {
+        this.video.pause();
+    }
+    
+    seek(time) {
+        this.video.currentTime = time;
+    }
+    
+    currentTime() {
+        return this.video.currentTime;
+    }
+    
+    duration() {
+        return this.video.duration;
+    }
+
+    durationChange(event) {
+        //check if the current time is inside the buffer
+        
+        
+    }
+}
+
+const VideoLog = (...args) => console.log("[VideoPlayer.js]", ...args);
+
+function waitForObjectState(check, interval = 100) {
+    return new Promise(resolve => {
+        const checkInterval = setInterval(() => {
+            if (check()) {
+                clearInterval(checkInterval);
+                resolve();
+            }
+        }, interval);
+    });
+}
